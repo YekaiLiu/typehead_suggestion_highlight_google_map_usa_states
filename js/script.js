acddcd252ca3a0cfe9ca359354e670912882929a
YekaiLiu <yl4241@columbia.edu>
2021-08-30 02:47:23 -0400

    2021-08-30 02:47:22 (EDT)

---
+++ js/script.js
@@ -0,0 +1,92 @@
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.com/search?q=${selectData}`;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
+
+function myMap() {
+
+    var mapProp= {
+    center:new google.maps.LatLng(39.089077,-103.2069358 ),
+    zoom:5,
+    };
+    var googleMap = new google.maps.Map(document.getElementById("googleMap"),mapProp); 
+
+    // Define the LatLng coordinates for the polygon's path.
+    const Alabama = [
+        
+        { lat: 35.0041, lng: -88.1955 },
+        { lat: 31.7947, lng: -85.1358 },
+        { lat: 32.2593, lng: -84.8927 },
+        { lat: 30.4404, lng: -87.3688 },
+        { lat: 34.9107, lng: -88.1461},
+    ];
+
+
+    // Construct the polygon.
+    const newpolygon = new google.maps.Polygon({
+        paths: Alabama,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+    });
+
+    newpolygon.setMap(googleMap);
+
+    }
---
+++ python/process.py
@@ -0,0 +1,8 @@
+with open('uspolygon.txt') as fp:
+    Lines = fp.readlines()
+
+# Strips the newline character
+    for line in Lines:
+
+        if 
+         print(line.strip())
